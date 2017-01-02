2c8eeaa934872ae66413428e524c53bd78de47a4
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2017-01-02 12:30:25 -0300

    Updates script

--- guga.js
+++ guga.js
@@ -4,14 +4,13 @@ var bip38 = require('bip38');
 var bitcore = require('bitcore');
 var Address = bitcore.Address;
 var networks = bitcore.networks;
-var WalletKey = bitcore.WalletKey;
 var Bip38 = new bip38();
 
 
 var addr = process.argv[2];
 var epk = process.argv[3];
-var a = new Address(addr);
-if (!addr || !a.isValid() || !epk) {
+var a = Address.isValid(addr, 'livenet');
+if (!addr || !a || !epk) {
   console.log("Usage: guga.js address privateKey(WIF)");
   process.exit(1);
 }
@@ -30,16 +29,18 @@ for (i in passArr) {
 
   try {
     var pk = Bip38.decrypt(epk, pass);
-    var wk = new WalletKey({network:networks.livenet});
-    wk.fromObj({priv:pk});
-    var o = wk.storeObj();
-    if (o.addr === addr) {
+console.log('#### Encrypted PrivateKey', epk);
+console.log('#### PrivateKey', pk);
+    var privateKey = new bitcore.PrivateKey(pk);
+    var publicKey = privateKey.toPublicKey();
+    var a = publicKey.toAddress('livenet').toString();
+console.log('#### Address', a);
+    if (a === addr) {
       console.log('##############   FOUND Address:' + addr + ' with password:' + pass);
       process.exit(1);
     }
   } catch (e) {
-console.log('[guga.js.40]',e); //TODO
-    console.log('.');
+    console.log('ERROR',e); //TODO
   }
 }
 
